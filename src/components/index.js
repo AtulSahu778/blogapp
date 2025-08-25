21ccfd1c54fdbde458c50b30da3442f87be563cf
Atul Sahu <127933681+AtulSahu778@users.noreply.github.com>
2025-08-25 19:41:32 +0530

    made components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./LogoutBtn"
+import Input from "./Input"
 
-export {Header, Footer}
+export {Header, Footer, Container, Logo, LogoutBtn, Input}
